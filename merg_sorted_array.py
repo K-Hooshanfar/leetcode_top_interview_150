7c9c291f25ad9689736fcfb3c8a749a5f28948c3
Kiana Hooshanfar <k.hooshanfar02@gmail.com>
2024-10-23 02:04:29 +0330

    answer for day 1

---
+++ merg_sorted_array.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if m == 0:
+            nums1[:n] = nums2[:n]
+
+        for t in range(m):
+            for i in range(m - 1):
+                # print(A)
+                if nums1[i] > nums1[i + 1]:
+                    nums1[i], nums1[i + 1] = nums1[i + 1], nums1[i]
+
+        for j in range(n):
+            for t in range(m + j):
+                if nums1[t] > nums2[j]:
+                    nums1[t+1:m+j+1] = nums1[t:m+j]
+                    nums1[t] = nums2[j]
+                    break
+                else:
+                    nums1[m + j] = nums2[j]
+
+        print(nums1)
+
+
+
+
+
+# Example of how to run this:
+nums1 = [2]
+m = 1  # the number of initialized elements in nums1
+nums2 = []
+n = 0  # the number of elements in nums2
+
+sol = Solution()
+sol.merge(nums1, m, nums2, n)
