c824e947f1940deec05f690007e786ffda3cce53
Kiana Hooshanfar <k.hooshanfar02@gmail.com>
2024-11-17 16:32:03 +0330

    leetcode 75

---
+++ leetcode75/Kids With the Greatest Number of Candies.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candies = max(candies)
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= max_candies:
+                candies[i] = 'True'
+            else:
+                candies[i] = 'False'
+
+        return candies
+
+
+candies  = [2,3,5,1,3]
+extraCandies = 3
+sol = Solution()
+len = sol.kidsWithCandies(candies, extraCandies)
+print(len)
